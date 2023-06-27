fd50652b9a22d1992284b83339fdf5a6fc0e805d
Sorokovsky <Sorokovskys@ukr.net>
2023-06-27 23:52:59 +0300

    replace

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,8 +1,10 @@
 import browserSync from "browser-sync";
 import gulp from 'gulp';
-import { deleteAsync } from "del"; 'del';
+import { deleteAsync } from "del";
+import replace from "gulp-replace";
 export const plugins = {
     browserSync,
     gulp,
     del: deleteAsync,
+    replace
 };
