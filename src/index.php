da17d77689c52b6db29caace8e0f9139690fdf42
Sorokovsky <Sorokovskys@ukr.net>
2023-06-28 14:49:56 +0300

    start

--- src/index.php
+++ src/index.php
@@ -1,8 +1,12 @@
-<?php 
-?>
-<html lang="uk" >
-    <body>
-        <?php include ('blocks/header.php'); ?>
-        <script src='@assets/js/script.min.js'></script>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
 </html>
